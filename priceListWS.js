9053562d503edc72f12f9588b36d7a0eef1314c1
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2018-01-11 16:19:39 +0530

    Update priceListWS.js

--- priceListWS.js
+++ priceListWS.js
@@ -2,82 +2,82 @@ module.exports = [
   {
     weight: 1,
     size: 10,
-    price: 25
+    price: 10
   }, {
     weight: 1,
     size: 20,
-    price: 30
+    price: 15
   }, {
     weight: 1,
     size: 50,
-    price: 70
+    price: 35
   }, {
     weight: 1,
     size: 100,
-    price: 100
+    price: 45
   }, {
     weight: 3,
     size: 10,
-    price: 40
+    price: 20
   }, {
     weight: 3,
     size: 20,
+    price: 25
+  }, {
+    weight: 3,
+    size: 50,
     price: 50
   }, {
     weight: 3,
-    size: 50,
-    price: 120
-  }, {
-    weight: 3,
     size: 100,
-    price: 150
+    price: 55
   }, {
     weight: 5,
     size: 10,
-    price: 25
+    price: 10
   }, {
     weight: 5,
     size: 20,
-    price: 30
+    price: 15
   }, {
     weight: 5,
+    size: 50,
+    price: 35
+  }, {
+    weight: 5,
+    size: 100,
+    price: 45
+  }, {
+    weight: 10,
+    size: 10,
+    price: 40
+  }, {
+    weight: 10,
+    size: 20,
+    price: 45
+  }, {
+    weight: 10,
     size: 50,
     price: 70
   }, {
-    weight: 5,
-    size: 100,
-    price: 100
-  }, {
-    weight: 10,
-    size: 10,
-    price: 90
-  }, {
-    weight: 10,
-    size: 20,
-    price: 100
-  }, {
-    weight: 10,
-    size: 50,
-    price: 250
-  }, {
     weight: 10,
     size: 100,
-    price: 270
+    price: 75
   }, {
     weight: 100,
     size: 10,
-    price: 150
+    price: 55
   }, {
     weight: 100,
     size: 20,
-    price: 180
+    price: 60
   }, {
     weight: 100,
     size: 50,
-    price: 340
+    price: 80
   }, {
     weight: 100,
     size: 100,
-    price: 390
+    price: 85
   }
 ]
