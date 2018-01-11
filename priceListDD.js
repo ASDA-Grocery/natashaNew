2dff9e0c96f0798df624b9c67bf09459367bcaa9
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2018-01-11 16:22:43 +0530

    Update priceListDD.js

--- priceListDD.js
+++ priceListDD.js
@@ -2,83 +2,83 @@ module.exports = [
   {
     distance: 200,
     days: 10,
-    price: 80
-  }, {
-    distance: 200,
-    days: 30,
-    price: 60
-  }, {
-    distance: 200,
-    days: 90,
     price: 40
   }, {
     distance: 200,
+    days: 30,
+    price: 30
+  }, {
+    distance: 200,
+    days: 90,
+    price: 20
+  }, {
+    distance: 200,
+    days: 1000,
+    price: 10
+  }, {
+    distance: 600,
+    days: 10,
+    price: 55
+  }, {
+    distance: 600,
+    days: 30,
+    price: 45
+  }, {
+    distance: 600,
+    days: 90,
+    price: 40
+  }, {
+    distance: 600,
     days: 1000,
     price: 25
   }, {
-    distance: 600,
+    distance: 1000,
     days: 10,
-    price: 150
+    price: 35
   }, {
-    distance: 600,
+    distance: 1000,
     days: 30,
-    price: 100
+    price: 45
   }, {
-    distance: 600,
+    distance: 1000,
     days: 90,
+    price: 55
+  }, {
+    distance: 1000,
+    days: 1000,
+    price: 65
+  }, {
+    distance: 2000,
+    days: 10,
     price: 80
   }, {
-    distance: 600,
+    distance: 2000,
+    days: 30,
+    price: 70
+  }, {
+    distance: 2000,
+    days: 90,
+    price: 60
+  }, {
+    distance: 2000,
     days: 1000,
     price: 50
   }, {
-    distance: 1000,
+    distance: 3000,
     days: 10,
+    price: 85
+  }, {
+    distance: 3000,
+    days: 30,
+    price: 75
+  }, {
+    distance: 3000,
+    days: 90,
     price: 70
   }, {
-    distance: 1000,
-    days: 30,
-    price: 100
-  }, {
-    distance: 1000,
-    days: 90,
-    price: 150
-  }, {
-    distance: 1000,
-    days: 1000,
-    price: 190
-  }, {
-    distance: 2000,
-    days: 10,
-    price: 350
-  }, {
-    distance: 2000,
-    days: 30,
-    price: 200
-  }, {
-    distance: 2000,
-    days: 90,
-    price: 160
-  }, {
-    distance: 2000,
-    days: 1000,
-    price: 120
-  }, {
-    distance: 3000,
-    days: 10,
-    price: 420
-  }, {
-    distance: 3000,
-    days: 30,
-    price: 250
-  }, {
-    distance: 3000,
-    days: 90,
-    price: 200
-  }, {
     distance: 3000,
     days: 1000,
-    price: 150
+    price: 55
   }
 
 ]
