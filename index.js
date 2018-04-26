8b8bb435b68a3306dd8d1098d5a98c5ae47ca4a5
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2018-04-26 10:31:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -309,7 +309,8 @@ app.post('/callWebhook', function(req, res) {
         else{
             speech = 'Sorry! This slot is not available.'
         }
-        var pickupSlot = slotStartTime + ' ' + timeOfDay.toUpperCase() + ' - ' + (slotStartTime+1) + ' ' + timeOfDay.toUpperCase()
+        var slotEndTime = slotStartTime+1;
+        var pickupSlot = slotStartTime + ' ' + timeOfDay.toUpperCase() + ' - ' + slotEndTime + ' ' + timeOfDay.toUpperCase()
         var queryString = 'http://54.183.205.111:3006/bookPickupSlot?data='+JSON.stringify(pickupSlot)+''
         superagent
           .get(queryString)
